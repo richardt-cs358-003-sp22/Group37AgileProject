54cce21528e5ba665c4bcd7ccb96bc46d9bfd877
Matthew Pomes <the10thwiz@gmail.com>
2022-04-21 14:11:02 -0500

    Add testing location lookup

---
+++ public/javascripts/maps.js
@@ -0,0 +1,31 @@
+/*
+ * maps.js
+ * Copyright (C) 2022 matthew <matthew@WINDOWS-05HIC4F>
+ *
+ * Distributed under terms of the MIT license.
+ */
+(function () {
+  "use strict";
+  let lookup = (text, callback) => {
+    // https://photon.komoot.io/api/?q=chicago&osm_tag=place:city&lat=52.3879&lon=13.0582
+    $.get(
+      "https://photon.komoot.io/api/?q=" +
+        text +
+        "&osm_tag=place:city&lat=52.3879&lon=13.0582"
+    ).done((json) => {
+      console.log(json);
+      let obj = {
+        name: json.features[0].properties.name,
+        lat: json.features[0].geometry.coordinates[1],
+        lon: json.features[0].geometry.coordinates[0],
+        country: json.features[0].properties.countrycode,
+      };
+      callback(obj);
+    });
+  };
+  $('#search_bar').keyup((e) => {
+    lookup(e.target.value, (e) => {
+      console.log(e)
+    });
+  });
+})();
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET reservations page */
+router.get('/maps', function(req, res, next) {
+  res.render('map', { title: 'Map Example' });
+});
+
 module.exports = router;
